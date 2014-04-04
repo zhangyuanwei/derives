4554798e07d07c1de1dba3d6c34f4b31824ce611
zhangyuanwei <zhangyuanwei1988@gmail.com>
2014-04-04 17:28:23 +0800

    add source

---
+++ test.js
@@ -0,0 +1,49 @@
+var ClassA = derive(Object, function(_super) {
+    this.name = "A";
+    console.log("constructor from ClassA");
+    console.log("this.name:", this.name);
+}, {
+    fn1: function(_super) {
+        console.log("fn1 from ClassA arguments.length:", arguments.length);
+        console.log("this.name:", this.name);
+    }
+});
+
+var ClassB = derive(ClassA, function(_super) {
+    _super();
+    this.name = "B";
+    console.log("constructor from ClassB");
+    console.log("this.name:", this.name);
+}, {
+    fn1: function(_super) {
+        _super();
+        console.log("fn1 from ClassB arguments.length:", arguments.length);
+        console.log("this.name:", this.name);
+    }
+});
+
+var ClassC = derive(ClassB, function(_super) {
+    _super();
+    this.name = "C";
+    console.log("constructor from ClassC");
+    console.log("this.name:", this.name);
+}, {
+    fn1: function(_super) {
+        _super();
+        console.log("fn1 from ClassC arguments.length:", arguments.length);
+        console.log("this.name:", this.name);
+    }
+});
+
+var ClassD = derive(ClassC, ClassB, {
+    fn1: function(_super) {
+        _super();
+        console.log("fn1 from ClassD arguments.length:", arguments.length);
+        console.log("this.name:", this.name);
+    }
+});
+
+
+//(new ClassC()).fn1();
+(new ClassD()).fn1();
+
